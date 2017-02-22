be707601d1e84f7a3aba78ed923423a2698c3fc5
AllisonBasore <allison.basore@students.olin.edu>
2017-02-22 13:57:23 -0500

    "Turning in the Geo Toolbox"

--- mbta_finder.py
+++ mbta_finder.py
@@ -1,5 +1,5 @@
 """
-Geocoding and Web APIs Project Toolbox exercise
+Geocoding and Web APIs Project Toolbox exercisecd
 
 Find the MBTA stops closest to a given location.
 
@@ -10,32 +10,106 @@ https://sites.google.com/site/sd15spring/home/project-toolbox/geocoding-and-web-
 from urllib.request import urlopen
 import json
 
+#MYAPI =  AIzaSyDNoQSj_sKLeWGPNU5WjqfAtZzgZ9a7TRc 
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
 GMAPS_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 MBTA_BASE_URL = "http://realtime.mbta.com/developer/api/v2/stopsbylocation"
 MBTA_DEMO_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
 
+GoogleAPI = 'AIzaSyAox26uPgX2Gv-A0dNWiYN3D3ZuFQ6VKUg' 
+ExampleGoogleCall = 'https://maps.googleapis.com/maps/api/geocode/json?address=1000+Olin+Way,+Needham,+MA&key=AIzaSyAox26uPgX2Gv-A0dNWiYN3D3ZuFQ6VKUg'
 
 # A little bit of scaffolding if you want to use it
 
-def get_json(url):
-    """
-    Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
-    """
-    pass
+from urllib.request import urlopen
+import json
+from pprint import pprint
+#originial below
+#url = "https://maps.googleapis.com/maps/api/geocode/json?address=Fenway%20Park"
+#f = urlopen(url)
+#response_text = f.read()
+#response_data = json.loads(str(response_text, "utf-8"))
 
+
+url = 'https://maps.googleapis.com/maps/api/geocode/json?address=543+Main+Street,+Burdick,+KS&key=AIzaSyAox26uPgX2Gv-A0dNWiYN3D3ZuFQ6VKUg'
+f = urlopen(url)
+response_text = f.read()
+response_data = json.loads(str(response_text, "utf-8"))
+
+
+api_Tstations = 'yGzxTxX6v02HXFVwHy1GFA' 
+#pprint(response_data)
+
+#print(response_data["results"][0]["formatted_address"])
+lat = response_data["results"][0]["geometry"]["location"]["lat"]
+lng = response_data["results"][0]["geometry"]["location"]["lng"]
+m = lat,lng
+
+
+#print(response_data["results"][0]["geometry"][0]["location"][0]["lat"][0])
 
 def get_lat_long(place_name):
     """
-    Given a place name or address, return a (latitude, longitude) tuple
+    Given a place name or address as a string, return a (latitude, longitude) tuple
     with the coordinates of the given place.
 
     See https://developers.google.com/maps/documentation/geocoding/
     for Google Maps Geocode API URL formatting requirements.
     """
-    pass
+    if place_name == 'Fenway':
+        url = "https://maps.googleapis.com/maps/api/geocode/json?address=Fenway%20Park"
+        f = urlopen(url)
+        response_text = f.read()
+        response_data = json.loads(str(response_text, "utf-8"))
+        lat = response_data["results"][0]["geometry"]["location"]["lat"]
+        lng = response_data["results"][0]["geometry"]["location"]["lng"]
+        m = lat,lng
+        return m 
+    if place_name == 'Museum of Fine Art':
+        url = 'https://maps.googleapis.com/maps/api/geocode/json?address=465+Huntington+Ave,+Boston,+MA&key=AIzaSyAox26uPgX2Gv-A0dNWiYN3D3ZuFQ6VKUg'
+        f = urlopen(url)
+        response_text = f.read()
+        response_data = json.loads(str(response_text, "utf-8"))
+        lat = response_data["results"][0]["geometry"]["location"]["lat"]
+        lng = response_data["results"][0]["geometry"]["location"]["lng"]
+        m = lat,lng
+        return m 
+    if place_name == 'Olin':
+        url = 'https://maps.googleapis.com/maps/api/geocode/json?address=1000+Olin+Way,+Needham,+MA&key=AIzaSyAox26uPgX2Gv-A0dNWiYN3D3ZuFQ6VKUg'
+        f = urlopen(url)
+        response_text = f.read()
+        response_data = json.loads(str(response_text, "utf-8"))
+        lat = response_data["results"][0]["geometry"]["location"]["lat"]
+        lng = response_data["results"][0]["geometry"]["location"]["lng"]
+        m = lat,lng
+        return m
+    if place_name == 'Needham':
+        url = 'https://maps.googleapis.com/maps/api/geocode/json?address=1471+Highland+Ave,+Needham,+MA&key=AIzaSyAox26uPgX2Gv-A0dNWiYN3D3ZuFQ6VKUg'
+        f = urlopen(url)
+        response_text = f.read()
+        response_data = json.loads(str(response_text, "utf-8"))
+        lat = response_data["results"][0]["geometry"]["location"]["lat"]
+        lng = response_data["results"][0]["geometry"]["location"]["lng"]
+        m = lat,lng
+        return m    
+    sep = place_name.split()
+    to_add = ''
+    for i in sep:
+        to_add = to_add + i
+        to_add = to_add + '+'
+    to_add = to_add[0:-1]
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+    Gapikey = 'AIzaSyAox26uPgX2Gv-A0dNWiYN3D3ZuFQ6VKUg'
+    url = url + to_add + '&key=' + Gapikey
+    f = urlopen(url)
+    response_text = f.read()
+    response_data = json.loads(str(response_text, "utf-8"))
+    lat = response_data["results"][0]["geometry"]["location"]["lat"]
+    lng = response_data["results"][0]["geometry"]["location"]["lng"]
+    m = lat,lng
+    return m
+
 
 
 def get_nearest_station(latitude, longitude):
@@ -46,12 +120,36 @@ def get_nearest_station(latitude, longitude):
     See http://realtime.mbta.com/Portal/Home/Documents for URL
     formatting requirements for the 'stopsbylocation' API.
     """
-    pass
 
+    MBTAurl = "http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key=yGzxTxX6v02HXFVwHy1GFA"
+    customloc = '&lat=' + str(latitude) + '&lon=' + str(longitude) + "&format=json"
+    f = urlopen(MBTAurl+customloc)
+    response_text = f.read()
+    response_data = json.loads(str(response_text, "utf-8"))
+    station = response_data["stop"][0]["stop_name"]
+    distanceto = response_data["stop"][0]["distance"]
 
-def find_stop_near(place_name):
+    #esponse_data["stop"][0]["geometry"]["location"]["lat"]
+    return station, distanceto
+
+def find_stop_near():
     """
     Given a place name or address, print the nearest MBTA stop and the
     distance from the given place to that stop.
     """
-    pass
+    input_loc = input('What address or major location would you like to search?   ')
+    latlong = get_lat_long(input_loc)
+    lat = str(latlong[0])
+    lon = str(latlong[1])
+    nearest = get_nearest_station(lat,lon) #nearest is a tuple 
+    print("The closest station is:   " + str(nearest[0]))
+    print("This station is " + str(nearest[1]) + '  miles away.')
+
+#print(get_lat_long('1000 Olin Way, Needham, MA'))
+
+find_stop_near()
+
+#find_stop_near()
+#APP_ID = 163709507468731
+#APP_SECRET = d794c6b698f11aa343424c4340cdbd6e
+#https://graph.facebook.com/walmart/posts/?key=value&access_token=163709507468731|d794c6b698f11aa343424c4340cdbd6e
